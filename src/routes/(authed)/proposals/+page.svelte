cbf686015e93a9428a0e26f1dcec8d168c3af4b5
Tzu-Sheng Kuo <awinder0230@gmail.com>
2025-06-02 17:36:25 -0400

    Proposal table

--- src/routes/(authed)/proposals/+page.svelte
+++ src/routes/(authed)/proposals/+page.svelte
@@ -1,1 +1,50 @@
-<h1>proposals</h1>
+<script lang="ts">
+	// import ui components
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Table from '$lib/components/ui/table/index.js';
+
+	// import lucide icons
+	import ArrowBigUpIcon from '@lucide/svelte/icons/arrow-big-up';
+	import ArrowBigDownIcon from '@lucide/svelte/icons/arrow-big-down';
+</script>
+
+<div class="flex h-screen w-full flex-col">
+	<div class="p-2">
+		<h2 class="p-2 text-xl font-bold">Proposals</h2>
+	</div>
+	<Separator />
+	<div class="h-full p-2">
+		<Table.Root>
+			<Table.Caption>A list of active proposals.</Table.Caption>
+			<Table.Header>
+				<Table.Row>
+					<Table.Head class="w-1/3">Title</Table.Head>
+					<Table.Head>Initiator</Table.Head>
+					<Table.Head>Last Edited By</Table.Head>
+					<Table.Head>Test Cases</Table.Head>
+					<Table.Head>Voting</Table.Head>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				<Table.Row>
+					<Table.Cell class="font-medium">What to do when the channels are irrelevant</Table.Cell>
+					<Table.Cell>filzzz</Table.Cell>
+					<Table.Cell>xacaranda</Table.Cell>
+					<Table.Cell>
+						<span class="text-discord-green mr-2">1</span> /
+						<span class="text-discord-pink mx-2">1</span> /
+						<span class=" ml-2">1</span>
+					</Table.Cell>
+					<Table.Cell>
+						<div class="flex items-center">
+							<ArrowBigUpIcon class="mr-2 size-4" />
+							<p class="mr-8">0</p>
+							<ArrowBigDownIcon class="mr-2 size-4" />
+							<p>0</p>
+						</div>
+					</Table.Cell>
+				</Table.Row>
+			</Table.Body>
+		</Table.Root>
+	</div>
+</div>
