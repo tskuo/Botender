eea77d14e7a24490c29bb605b5af37a622fb8b25
Tzu-Sheng Kuo <awinder0230@gmail.com>
2025-06-01 23:01:33 -0400

    Case and case cards

---
+++ src/lib/components/CaseCard.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	// import ui components
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { Badge } from '$lib/components/ui/badge/index.js';
+
+	// import lucide icons
+	import HashIcon from '@lucide/svelte/icons/hash';
+	import UserRoundIcon from '@lucide/svelte/icons/user-round';
+	import WrenchIcon from '@lucide/svelte/icons/wrench';
+	import BotIcon from '@lucide/svelte/icons/bot';
+</script>
+
+<Dialog.Root>
+	<Dialog.Trigger>
+		<Card.Root class="text-left">
+			<Card.Header>
+				<!-- <Card.Title></Card.Title> -->
+				<Card.Description class="flex justify-between">
+					<Badge variant="outline">worth checking</Badge>
+					<Badge variant="outline">test case</Badge>
+				</Card.Description>
+			</Card.Header>
+			<Card.Content>
+				<div class="mb-2 flex items-center">
+					<HashIcon class="mr-2 size-4" />
+					<h3 class="font-semibold">Channel</h3>
+				</div>
+				<div class="mb-3 flex">
+					<UserRoundIcon class="mt-1 mr-2 size-4 flex-none" />
+					<p>
+						Hello, everyone! My name is Min Park (she/her), a Ph.D. student from Korea Institute of
+						Science and Technology (KAIST), as well as the moderator of this server.
+					</p>
+				</div>
+				<div class="mb-2 flex items-center">
+					<WrenchIcon class="mr-2 size-4" />
+					<h3 class="font-semibold">Welcome Newcomers</h3>
+				</div>
+				<div class="mb-3 flex">
+					<BotIcon class="mt-1 mr-2 size-4 flex-none" />
+					<p>
+						Since you love games and musicals, you might be interested in joining channels like
+						#sports, #plants, and #cafe!
+					</p>
+				</div>
+			</Card.Content>
+			<!-- <Card.Footer>
+				<p>Card Footer</p>
+			</Card.Footer> -->
+		</Card.Root>
+	</Dialog.Trigger>
+	<Dialog.Content>
+		<Dialog.Header>
+			<Dialog.Title>Are you sure absolutely sure?</Dialog.Title>
+			<Dialog.Description>
+				This action cannot be undone. This will permanently delete your account and remove your data
+				from our servers.
+			</Dialog.Description>
+		</Dialog.Header>
+	</Dialog.Content>
+</Dialog.Root>
--- src/routes/(authed)/cases/+page.svelte
+++ src/routes/(authed)/cases/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	// import ui components
 	import { Separator } from '$lib/components/ui/separator/index.js';
+	import CaseCard from '$lib/components/CaseCard.svelte';
 </script>
 
 <div class="flex h-screen w-full flex-col">
@@ -8,4 +9,12 @@
 		<h2 class="p-2 text-xl font-bold">Cases</h2>
 	</div>
 	<Separator />
+	<div class="grid auto-rows-fr grid-cols-1 gap-2 p-4 md:grid-cols-2 lg:grid-cols-3">
+		<CaseCard />
+		<CaseCard />
+		<CaseCard />
+		<CaseCard />
+		<CaseCard />
+		<CaseCard />
+	</div>
 </div>
