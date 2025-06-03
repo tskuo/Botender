faaffa2063928d7eb396f76520a4eeefc29c8963
Tzu-Sheng Kuo <awinder0230@gmail.com>
2025-06-03 00:22:10 -0400

    Manuel enter case sheet

--- src/routes/(authed)/proposals/[proposalId]/+page.svelte
+++ src/routes/(authed)/proposals/[proposalId]/+page.svelte
@@ -6,9 +6,31 @@
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Carousel from '$lib/components/ui/carousel/index.js';
+	import * as Sheet from '$lib/components/ui/sheet/index.js';
+
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
 
 	// import lucide icons
 	import ChevronLeftIcon from '@lucide/svelte/icons/chevron-left';
+
+	// sync the sheet
+	let rightCol: HTMLDivElement | null = null;
+	let sheetWidth = 0;
+
+	// Update width on mount and on resize
+	function updateSheetWidth() {
+		if (rightCol) {
+			sheetWidth = rightCol.offsetWidth;
+		}
+	}
+	import { onMount } from 'svelte';
+	onMount(() => {
+		updateSheetWidth();
+		window.addEventListener('resize', updateSheetWidth);
+		return () => window.removeEventListener('resize', updateSheetWidth);
+	});
 </script>
 
 <div class="flex h-screen w-full flex-col">
@@ -20,7 +42,7 @@
 		<h2 class="p-2 text-xl font-bold">Proposal: What to do when the channels are irrelevant</h2>
 	</div>
 	<Separator />
-	<div class="grid flex-1 md:grid-cols-5">
+	<div class="grid flex-auto md:grid-cols-5">
 		<div class="border-r p-2 md:col-span-2">
 			<div class="p-2">
 				<h3>Description</h3>
@@ -47,67 +69,101 @@
 				</p>
 			</div>
 		</div>
-		<div class="h-full flex-col md:col-span-3">
-			<div class="p-4 md:h-1/2">
-				<h3>Check test cases</h3>
-				<p class="text-muted-foreground mb-1">
-					2 people have collaboratively propose the following edit
-				</p>
-				<Carousel.Root
-					opts={{
-						align: 'start'
-					}}
-					class="mx-auto w-4/5 max-w-screen md:w-5/6"
-				>
-					<Carousel.Content>
-						{#each Array(5) as _, i (i)}
-							<Carousel.Item class="xl:basis-1/2">
-								<div class="p-1">
-									<CaseCard
-										id={'xxx'}
-										channel={'#introduction'}
-										userMessage={'This is Sebastian writing. I am currently building up a research group in Berlin with a focus on the intersection of data engineering and ML. We have a postdoc opening in my group, which I would like to share here:'}
-										triggeredTask={'Welcome Newcomers'}
-										botResponse={'Welcome to our community! We have channels for sports, plants, and cafe discussions. Feel free to join any of them!'}
-									/>
-								</div>
-							</Carousel.Item>
-						{/each}
-					</Carousel.Content>
-					<Carousel.Previous />
-					<Carousel.Next />
-				</Carousel.Root>
+		<div class="flex h-full flex-col overflow-hidden md:col-span-3" bind:this={rightCol}>
+			<div class="flex-1 overflow-hidden">
+				<div class="p-4 md:h-1/2">
+					<h3>Check test cases</h3>
+					<p class="text-muted-foreground mb-1">
+						2 people have collaboratively propose the following edit
+					</p>
+					<Carousel.Root
+						opts={{
+							align: 'start'
+						}}
+						class="mx-auto w-4/5 max-w-screen md:w-5/6"
+					>
+						<Carousel.Content>
+							{#each Array(5) as _, i (i)}
+								<Carousel.Item class="xl:basis-1/2">
+									<div class="p-1">
+										<CaseCard
+											id={'xxx'}
+											channel={'#introduction'}
+											userMessage={'This is Sebastian writing. I am currently building up a research group in Berlin with a focus on the intersection of data engineering and ML. We have a postdoc opening in my group, which I would like to share here:'}
+											triggeredTask={'Welcome Newcomers'}
+											botResponse={'Welcome to our community! We have channels for sports, plants, and cafe discussions. Feel free to join any of them!'}
+										/>
+									</div>
+								</Carousel.Item>
+							{/each}
+						</Carousel.Content>
+						<Carousel.Previous />
+						<Carousel.Next />
+					</Carousel.Root>
+				</div>
+				<Separator />
+				<div class="p-4 md:h-1/2">
+					<h3>Check other cases for possible side effects from the proposed edit</h3>
+					<p class="text-muted-foreground mb-1">
+						5 cases have been suggested. You may add (+) relevant cases to the test cases.
+					</p>
+					<Carousel.Root
+						opts={{
+							align: 'start'
+						}}
+						class="mx-auto w-4/5 max-w-screen md:w-5/6"
+					>
+						<Carousel.Content>
+							{#each Array(5) as _, i (i)}
+								<Carousel.Item class="xl:basis-1/2">
+									<div class="p-1">
+										<CaseCard
+											id={'xxx'}
+											channel={'#introduction'}
+											userMessage={'This is Sebastian writing. I am currently building up a research group in Berlin with a focus on the intersection of data engineering and ML. We have a postdoc opening in my group, which I would like to share here:'}
+											triggeredTask={'Welcome Newcomers'}
+											botResponse={'Welcome to our community! We have channels for sports, plants, and cafe discussions. Feel free to join any of them!'}
+										/>
+									</div>
+								</Carousel.Item>
+							{/each}
+						</Carousel.Content>
+						<Carousel.Previous />
+						<Carousel.Next />
+					</Carousel.Root>
+				</div>
 			</div>
-			<Separator />
-			<div class="p-4 md:h-1/2">
-				<h3>Check other cases for possible side effects from the proposed edit</h3>
-				<p class="text-muted-foreground mb-1">
-					5 cases have been suggested. You may add (+) relevant cases to the test cases.
-				</p>
-				<Carousel.Root
-					opts={{
-						align: 'start'
-					}}
-					class="mx-auto w-4/5 max-w-screen md:w-5/6"
-				>
-					<Carousel.Content>
-						{#each Array(5) as _, i (i)}
-							<Carousel.Item class="xl:basis-1/2">
-								<div class="p-1">
-									<CaseCard
-										id={'xxx'}
-										channel={'#introduction'}
-										userMessage={'This is Sebastian writing. I am currently building up a research group in Berlin with a focus on the intersection of data engineering and ML. We have a postdoc opening in my group, which I would like to share here:'}
-										triggeredTask={'Welcome Newcomers'}
-										botResponse={'Welcome to our community! We have channels for sports, plants, and cafe discussions. Feel free to join any of them!'}
-									/>
-								</div>
-							</Carousel.Item>
-						{/each}
-					</Carousel.Content>
-					<Carousel.Previous />
-					<Carousel.Next />
-				</Carousel.Root>
+			<div class="w-full shrink-0">
+				<Sheet.Root>
+					<Sheet.Trigger class="w-full border-t py-2 pl-4 text-left">
+						<h3>Check other cases manually</h3>
+					</Sheet.Trigger>
+					<Sheet.Content
+						class="ml-auto"
+						side="bottom"
+						style="width: {sheetWidth}px; max-width: 100vw;"
+					>
+						<Sheet.Header>
+							<Sheet.Title>Edit profile</Sheet.Title>
+							<Sheet.Description>
+								Make changes to your profile here. Click save when you're done.
+							</Sheet.Description>
+						</Sheet.Header>
+						<div class="grid flex-1 auto-rows-min gap-6 px-4">
+							<div class="grid gap-3">
+								<Label for="name" class="text-right">Name</Label>
+								<Input id="name" value="Pedro Duarte" />
+							</div>
+							<div class="grid gap-3">
+								<Label for="username" class="text-right">Username</Label>
+								<Input id="username" value="@peduarte" />
+							</div>
+						</div>
+						<Sheet.Footer>
+							<Sheet.Close class={buttonVariants({ variant: 'outline' })}>Save changes</Sheet.Close>
+						</Sheet.Footer>
+					</Sheet.Content>
+				</Sheet.Root>
 			</div>
 		</div>
 	</div>
